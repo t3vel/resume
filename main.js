4d4662a1c8706abcb31400bb4466dfcc5ecad4b7
Maks <example@gmail.com>
2024-11-13 14:25:50 +0200

    Fix bugs with aside

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 function switchLanguage() {
-    const contentEn = document.getElementById("content-en");
-    const contentUa = document.getElementById("content-ua");
-    const sidebarEn = document.getElementById("sidebar-en");
-    const sidebarUa = document.getElementById("sidebar-ua");
-    const button = document.getElementById("language-switch");
+    // Перемикання видимості основного контенту
+    const contentEn = document.getElementById('content-en');
+    const contentUa = document.getElementById('content-ua');
+    const sidebarEn = document.querySelector('.sidebar');
+    const sidebarUa = document.getElementById('sidebar-ua');
 
-    if (contentEn.style.display === "none") {
-        contentEn.style.display = "block";
-        contentUa.style.display = "none";
-        sidebarEn.style.display = "block";
-        sidebarUa.style.display = "none";
-        button.textContent = "UA";
+    if (contentEn.style.display === 'none') {
+
+        contentEn.style.display = 'block';
+        contentUa.style.display = 'none';
+
+        sidebarEn.style.display = 'block';
+        sidebarUa.style.display = 'none';
+
+        // Змінюємо текст кнопки на "UA"
+        document.getElementById('language-switch').innerText = 'UA';
     } else {
-        contentEn.style.display = "none";
-        contentUa.style.display = "block";
-        sidebarEn.style.display = "none";
-        sidebarUa.style.display = "block";
-        button.textContent = "EN";
+
+        contentEn.style.display = 'none';
+        contentUa.style.display = 'block';
+
+
+        sidebarEn.style.display = 'none';
+        sidebarUa.style.display = 'block';
+
+        document.getElementById('language-switch').innerText = 'EN';
     }
 }
